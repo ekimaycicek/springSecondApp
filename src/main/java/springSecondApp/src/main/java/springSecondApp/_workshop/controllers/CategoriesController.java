9fae0b2c7aafb31c3413f11dcd6ddc838daf74be
ekimaycicek <ekimaycicek@icloud.com>
2023-12-07 23:14:57 +0300

    new

--- src/main/java/springSecondApp/src/main/java/springSecondApp/_workshop/controllers/CategoriesController.java
+++ src/main/java/springSecondApp/src/main/java/springSecondApp/_workshop/controllers/CategoriesController.java
@@ -28,6 +28,7 @@ public class CategoriesController {
         return category.getId()+" ID' li kategori sisteme eklendi.";
     }
 
+
     @PutMapping
     public String update(@RequestBody Category updatedCategory){
         Category category = inCategoriesList
