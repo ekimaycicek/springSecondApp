0e45a7f7be0ec5a5b947497719a30c5e7e5f09e9
ekimaycicek <ekimaycicek@icloud.com>
2024-02-21 23:40:07 +0300

    new

--- src/main/java/springSecondApp/src/main/java/springSecondApp/_workshop/controllers/CategoriesController.java
+++ src/main/java/springSecondApp/src/main/java/springSecondApp/_workshop/controllers/CategoriesController.java
@@ -27,7 +27,6 @@ public class CategoriesController {
 
 
 
-
     @GetMapping("{id}")
     public Category getById(@PathVariable int id){
         Category category = inCategoriesList.stream().filter((p) -> p.getId() == id).findFirst().orElseThrow();
