a8177223750c396a11c0e775bffdbd421e385813
ekimaycicek <ekimaycicek@icloud.com>
2024-02-28 23:54:20 +0300

    new

--- src/main/java/springSecondApp/src/test/java/springSecondApp31/_workshop/ApplicationTests.java
+++ src/main/java/springSecondApp/src/test/java/springSecondApp31/_workshop/ApplicationTests.java
@@ -3,7 +3,6 @@ package springSecondApp.src.test.java.springSecondApp31._workshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 @SpringBootTest
 class ApplicationTests {
 
